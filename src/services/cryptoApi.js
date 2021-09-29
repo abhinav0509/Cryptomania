1f69effa47cf144c3ce27fbe39932d7013eb540a
Abhinav Raj <abhinav.raj0509@gmail.com>
2021-09-29 23:03:03 +0530

    Add files via upload

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,1 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "8421aa08cbmsh0707b5a24382522p1de962jsn697fb95505d6",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsApiHeaders = {
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "8421aa08cbmsh0707b5a24382522p1de962jsn697fb95505d6",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com/news";
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
